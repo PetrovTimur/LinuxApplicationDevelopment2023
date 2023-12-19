08b66c875a332a312d4fbaee383a1c5c30fd2db2
Timur Petrov <s02200459@gse.cs.msu.ru>
2023-12-19 17:02:11 +0000

    Add task 10

---
+++ 10_Testing/tests/NullTest.c
@@ -0,0 +1,13 @@
+#include "test.h"
+
+int main(int argc, char const *argv[])
+{
+	/* Clearing an NULL pointer is a no-op */
+
+	float *a = 0;
+	buf_clear(a);
+    TEST("clear empty", buf_size(a) == 0);
+    TEST("clear no-op", a == 0);
+
+	return 0;
+}
