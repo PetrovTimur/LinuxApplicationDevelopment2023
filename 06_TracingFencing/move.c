aa4feff0ef7d0d7ffe69318a1ea90a45af429a23
Timur Petrov <s02200459@gse.cs.msu.ru>
2023-12-20 16:24:36 +0000

    Add tests for task 6

--- 06_TracingFencing/move.c
+++ 06_TracingFencing/move.c
@@ -15,19 +15,19 @@ int main(int argc, char const *argv[])
 	FILE *f_in = fopen(argv[1], "rb");
 	if (f_in == NULL) {
 		perror("Error. Could not open infile");
-		exit(1);
+		return 1;
 	}
 
 	FILE *f_out = fopen(argv[2], "w");
 	if (f_out == NULL) {
 		perror("Error. Could not open outfile");
-		exit(2);
+		return 2;
 	}
 
 	struct stat file_status;
 	if (stat(argv[1], &file_status) < 0) {
         perror("Error. Could not get input file size");
-        exit(1);
+        return 3;
     }
     long infile_size = file_status.st_size;
 
@@ -40,12 +40,12 @@ int main(int argc, char const *argv[])
 		bytes_read = fread(buffer, sizeof(char), nmemb, f_in);
 		if (ferror(f_out)) {
 			perror("Error. Could not write to outfile");
-			exit(1);
+			return 4;
 		}
 
 		if (fwrite(buffer, sizeof(char), bytes_read, f_out) != bytes_read) {
 			fprintf(stderr, "%s\n", "Error. Could not write enough bytes");
-			exit(1);
+			return 5;
 		}
 
 		total_bytes += bytes_read;
@@ -53,12 +53,12 @@ int main(int argc, char const *argv[])
 
 	if (ferror(f_in)) {
 		perror("Error. Could not read from infile");
-		exit(1);
+		return 6;
 	}
 
     if (infile_size != total_bytes * sizeof(char)) {
     	fprintf(stderr, "%s\n", "Error. File sizes do not match. Abort moving");
-    	exit(1);
+    	return 7;
     }
 
 	fclose(f_in);
