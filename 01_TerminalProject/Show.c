70a6d768fc9faaadbaa43734a913ebbbfa8d6ea9
Timur Petrov <s02200459@gse.cs.msu.ru>
2023-12-22 23:07:45 +0000

    Modify task 1

--- 01_TerminalProject/Show.c
+++ 01_TerminalProject/Show.c
@@ -1,6 +1,8 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <locale.h>
 
 #define DX 3
 
@@ -10,38 +12,38 @@ int main(int argc, char* argv[]) {
                 return 1;
         }
 
-        FILE *f = fopen(argv[1], "r");
+        setlocale(LC_ALL, "");
+
+        FILE *f = fopen(argv[1], "rt");
         int fd = fileno(f);
 
         struct stat buf;
         fstat(fd, &buf);
         off_t size = buf.st_size;
 
-        printf("%ld\n", size);
-        char *data = (char*) malloc(size + 1);
-        size_t result = fread(data, 1, size, f);
-        printf("%ld\n", result);
-        // printf("%c, %d\n", data[size-3], data[size-1]);
-        data[size] = EOF;
+        // printf("%ld\n", size);
+        // char *data = (char*) malloc(size + 1);
+        // size_t result = fread(data, 1, size, f);
+        // printf("%ld\n", result);
+        // // printf("%c, %d\n", data[size-3], data[size-1]);
+        // data[size] = EOF;
 
-        fseek(f, 0, SEEK_SET);
+        // fseek(f, 0, SEEK_SET);
 
-        int count = 0, i = 0;
-        char ch;
-        while (data[i] != EOF) {
-                if (data[i] == '\n')
-                        count++;
-                i++;
-                // printf("%d\n", i);
-        }
+        int count = 0, i = 0, maxlen = 0;
+        // char ch;
+        // while (data[i] != EOF) {
+        //         if (data[i] == '\n')
+        //                 count++;
+        //         i++;
+        //         // printf("%d\n", i);
+        // }
 
-        count++;
-        printf("lines: %d\n", count);
-
-        fseek(f, 0, SEEK_SET);
+        // count++;
+        // printf("lines: %d\n", count);
 
         // count
-        char *lines[count];
+        // char *lines[count];
 
         char *buffer;
         size_t bufsize = 100;
@@ -49,23 +51,52 @@ int main(int argc, char* argv[]) {
 
         i = 0;
         // return 0;
-        lines[0] = (char*) malloc(bufsize * sizeof(char));
+        // lines[0] = (char*) malloc(bufsize * sizeof(char));
         buffer = (char *)malloc(bufsize * sizeof(char));
 
-        int zz;
-        while ((zz = getline(&(lines[i]),&bufsize,f)) != -1) {
-                // fputs(lines[i], stdout);
+        int len;
+        while ((len = getline(&buffer, &bufsize, f)) != -1) {
+                // fputs(buffer, stdout);
+                maxlen = len > maxlen ? len : maxlen;
                 i++;
-                lines[i] = (char*) malloc(100);
+                // lines[i] = (char*) malloc(100);
         }
 
-        // i = 0;
-        // for (i = 0; i < count; i++) {
-        //         fputs(lines[i], stdout);
-        // }
+        // printf("lines: %d, maxlen: %d\n", i, maxlen);
+        maxlen++;
+        
+        char *lines[i];
+        int lens[i];
+        fseek(f, 0, SEEK_SET);
+
+        for (int p = 0; p < i; p++) {
+                lines[p] = (char *) malloc(bufsize * sizeof(char));
+        }
+
+
+        i = 0;
+        while ((len = getline(&(lines[i]), &bufsize, f)) != -1) {
+                lens[i] = len;
+                // strncpy(lines[i], buffer, maxlen);
+                // fputs(buffer, stdout);
+                // fputs(lines[i], stdout);
+                // printf("%d, %d\n", len, lines[i][maxlen]);
+                // int succ = strncpy
+                i++;
+                // lines[i] = (char*) malloc(100);
+        }
+
+        count = i;
+        for (i = 0; i < count; i++) {
+                // printf("%c: %d\n", lines[1][i], lines[1][i]);
+                // printf("%d: %d\n", i, lens[i]);
+                // fputs(lines[i], stdout);
+                // printf("%c\n", lines[i][maxlen - 2]);
+        }
 
         WINDOW *win;
         int c = 0;
+        // printf("%s\n\n\n", "hi");
 
         initscr();
         noecho();
@@ -79,34 +110,59 @@ int main(int argc, char* argv[]) {
         box(win, 0, 0); 
         wmove(win, 1, 0);
 
-        char line[COLS];
-        printf("%d\n", COLS);
+        printf("%s\n", argv[1]);
 
+        int width = COLS - 2 * DX - 2;
+        int height = LINES - 2 * DX;
+        char line[width];
+
+        // printf("count = %d\n", count);
         i = 0;
+        int result;
         int j = 0;
         while((c = wgetch(win)) != 27) {
-                if (c == KEY_DOWN) {
+                // wclear(win);
+                // box(win, 1, 1);
+                if ((c == KEY_DOWN) && (i < count - height)) {
+                        // printf("%d, %d, %d\n", i, count, height);
                         i++;
                         // printf("%d\n", i);
-                } else if (c == KEY_RIGHT) {
+                } else if ((c == KEY_RIGHT) && (j < maxlen - width)) {
                         j++;
-                } else if (c == KEY_LEFT) {
+                } else if ((c == KEY_LEFT) && (j > 0)) {
                         j--;
-                } else if (c == KEY_UP) {
+                } else if ((c == KEY_UP) && (i > 0)) {
                         i--;
                 }
-                // wprintw(win, "  Key: %d, Name: %s\n", c, keyname(c));
-                for (int k = i; k < LINES - 2 * DX + i; k++) {
-                        result = snprintf(line, COLS, " %d: %s", k, lines[k] + j);
+                for (int k = i; k < height + i; k++) {
+                        result = snprintf(line, width, "%d: %s", k, lines[k] + j);
                         // printf("%d\n", result);
-                        wprintw(win, line);
+
+                        // if (result == width) {
+                        //         line[width - 1] = "\n";
+                        // }
+
+                        // int length = lens[k];
+                        // if (length - j >= width) {
+                        //         result = snprintf(line, width, " %d: %s", k, lines[k] + j);
+                        //         printf("%d\n", result);
+                        //         line[width - 1] = '\n';
+                        // } else {
+                        //         if (j >= length) {
+                        //                 result = snprintf(line, width, " %d: %s", k, lines[k] + lens[k] - 1);
+                        //         } else {
+                        //                 result = snprintf(line, width, " %d: %s", k, lines[k] + j);
+                        //         }
+                        // }
+                        mvwprintw(win, k - i + 1, 1, line);
                 }
                 box(win, 0, 0); 
                 wrefresh(win);
         }
         endwin();
 
-        free(data);
+        // free(data);
+        fclose(f);
 
         return 0;
 }
